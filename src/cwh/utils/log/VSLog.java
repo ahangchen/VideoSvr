bd7428c8e91c5a690c4118cc31291c148107b94b
ahangchen <1418659400@qq.com>
2016-01-15 15:47:09 +0800

    log增加tag

--- src/cwh/utils/log/VSLog.java
+++ src/cwh/utils/log/VSLog.java
@@ -15,68 +15,82 @@ import java.util.Calendar;
 public class VSLog {
     public static String logPath = "/media/Software/lab/data/";
     public static String EXT = ".log";
-    public static String INFO = "[I]";
     public static String DEBUG = "[D]";
     public static String ERROR = "[E]";
-    public static String WARN = "[W]";
 
     public static boolean LOG = true;
 
-
-    public static void i(String logStr) {
-        log(INFO, logStr);
-    }
 
     public static void d(String logStr) {
         log(DEBUG, logStr);
     }
 
-    public static void w(String logStr) {
-        log(WARN, logStr);
+    public static void d(String TAG, String logStr) {
+        log(TAG, DEBUG, logStr);
     }
+
 
     public static void e(String logStr) {
         log(ERROR, logStr);
+    }
+    public static void e(String tag, String logStr) {
+        log(tag, ERROR, logStr);
     }
 
     public static void e(String logStr, Throwable t) {
         err(logStr, t);
     }
+    public static void e(String tag, String logStr, Throwable t) {
+        err(logStr, t);
+    }
 
-
-    public static void log(String type, String logStr) {
-        String logName = Calendar.getInstance().get(Calendar.YEAR) + "-" + (Calendar.getInstance().get(Calendar.MONTH) + 1)
-                + "-" + Calendar.getInstance().get(Calendar.DAY_OF_MONTH) + EXT;
-        String log = format(logStr, type);
+    private static void printLog(String logFileName, String formatLog) {
         try {
-            FileOutputStream out = new FileOutputStream(new File(logPath + logName), true);
-            if (LOG) out.write(log.getBytes());
-            System.out.print(log);
+            FileOutputStream out = new FileOutputStream(new File(logPath + logFileName), true);
+            if (LOG) out.write(formatLog.getBytes());
+            System.out.print(formatLog);
             out.close();
         } catch (Exception e) {
             System.out.println();
         }
     }
 
-    private static String format(String log, String type) {
-        return DateUtils.formatCurTime() + " "
-                        + type
-                        + " : " + log + "\n";
-    }
-
-
-    public static void err(String logStr, Throwable throwable) {
+    public static void log(String tag, String type, String logStr) {
         String logName = Calendar.getInstance().get(Calendar.YEAR) + "-" + (Calendar.getInstance().get(Calendar.MONTH) + 1)
                 + "-" + Calendar.getInstance().get(Calendar.DAY_OF_MONTH) + EXT;
-        String log = format(logStr, ERROR);
+        String log = format(tag, logStr, type);
+
+        printLog(logName, log);
+    }
+
+    public static void log(String type, String logStr) {
+        String logName = Calendar.getInstance().get(Calendar.YEAR) + "-" + (Calendar.getInstance().get(Calendar.MONTH) + 1)
+                + "-" + Calendar.getInstance().get(Calendar.DAY_OF_MONTH) + EXT;
+        String log = format(logStr, type);
+        printLog(logName, log);
+    }
+
+    private static String format(String log, String type) {
+        return DateUtils.formatCurTime() + " "
+                + type
+                + " : " + log + "\n";
+    }
+
+    private static String format(String tag, String log, String type) {
+        return DateUtils.formatCurTime() + " "
+                + type + "[" + tag + "]" + " : "
+                + log + "\n";
+    }
+
+    private static void printErr(String logFileName, String formatLog, Throwable throwable) {
         try {
-            FileOutputStream out = new FileOutputStream(new File(logPath + logName), true);
-            if (LOG) out.write(log.getBytes());
-            System.out.print(log);
+            FileOutputStream out = new FileOutputStream(new File(logPath + logFileName), true);
+            if (LOG) out.write(formatLog.getBytes());
+            System.out.print(formatLog);
             StackTraceElement[] stacks = throwable.getStackTrace();
-            for (int i = 0; i < stacks.length; i++) {
-                if (LOG) out.write(format(stacks[i].toString(), ERROR).getBytes());
-                System.out.print(format(stacks[i].toString(), ERROR));
+            for (StackTraceElement stack : stacks) {
+                if (LOG) out.write(format(stack.toString(), ERROR).getBytes());
+                System.out.print(format(stack.toString(), ERROR));
             }
             out.close();
         } catch (Exception e) {
@@ -84,9 +98,27 @@ public class VSLog {
         }
     }
 
+    public static void err(String logStr, Throwable throwable) {
+        String logName = Calendar.getInstance().get(Calendar.YEAR) + "-" + (Calendar.getInstance().get(Calendar.MONTH) + 1)
+                + "-" + Calendar.getInstance().get(Calendar.DAY_OF_MONTH) + EXT;
+        String log = format(logStr, ERROR);
+        printErr(logName, log, throwable);
+    }
+
+    public static void err(String tag, String logStr, Throwable throwable) {
+        String logName = Calendar.getInstance().get(Calendar.YEAR) + "-" + (Calendar.getInstance().get(Calendar.MONTH) + 1)
+                + "-" + Calendar.getInstance().get(Calendar.DAY_OF_MONTH) + EXT;
+        String log = format(tag, logStr, ERROR);
+        printErr(logName, log, throwable);
+    }
+
     public static void main(String[] args) {
-        log(DEBUG, "TEST");
+        log("Vslog", DEBUG, "TEST");
         ThreadUtils.sleep(200);
+        log("VSLOG", ERROR, "TEST");
+        d("vslog", "debuglog");
+        e("vslog", "debuglog");
+        e("vslog", "debuglog", new Exception());
         log(DEBUG, "TEST");
         ThreadUtils.sleep(200);
         log(DEBUG, "TEST");
