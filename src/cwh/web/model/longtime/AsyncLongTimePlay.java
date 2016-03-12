d8d9bb3c89eb19352252a89711a3654bb3732909
ahangchen <1418659400@qq.com>
2016-03-12 17:58:15 +0800

    m3u8 format

--- src/cwh/web/model/longtime/AsyncLongTimePlay.java
+++ src/cwh/web/model/longtime/AsyncLongTimePlay.java
@@ -44,83 +44,84 @@ public class AsyncLongTimePlay implements Runnable {
         final String tsDir = CommonDefine.PLAY_BACK_DIR_PATH + File.separator + playBackParam.toString() + CommonDefine.LONG_TIME;
         SessionManager.getInstance().requestVideo(tsDir.replace(CommonDefine.DATA_PATH + File.separator, "") + File.separator + CommonDefine.LONG_TIME_M3U8,
                 sessionState, new SessionManager.CacheCallback() {
-            @Override
-            public void onOld(RequestState longTimeState) {
-                VSLog.d(TAG, "cached");
-                PlaybackHelper.responseString(context.getResponse(), ((LongTimeRes) longTimeState.getRes()).toJson(sessionState.getSessionId()));
-                context.complete();
-                VSLog.d(TAG, "on Complete");
-            }
+                    @Override
+                    public void onOld(RequestState longTimeState) {
+                        VSLog.d(TAG, "cached");
+                        PlaybackHelper.responseString(context.getResponse(), ((LongTimeRes) longTimeState.getRes()).toJson(sessionState.getSessionId()));
+                        context.complete();
+                        VSLog.d(TAG, "on Complete");
+                    }
 
-            @Override
-            public boolean onNew(RequestState requestState) {
-                FileUtils.mkdir(tsDir);
-                final String longTimeM3U8Path = tsDir + File.separator + CommonDefine.LONG_TIME_M3U8;
-                FileUtils.createFile(longTimeM3U8Path);
-                final int[][][] timeIntervals = LongTimeHelper.splitTime(
-                        playBackParam.getStartYear(), playBackParam.getStartMon(), playBackParam.getStartDay(),
-                        playBackParam.getStartHour(), playBackParam.getStartMin(), playBackParam.getStartSec(),
-                        playBackParam.getEndYear(), playBackParam.getEndMon(), playBackParam.getEndDay(),
-                        playBackParam.getEndHour(), playBackParam.getEndMin(), playBackParam.getEndSec());
-                // 数组实现执向引用的常引用
-                final String[] playBackPath = new String[1];
-                final boolean[] waitEnd = new boolean[1];
-                waitEnd[0] = false;
-                final int[] tsIndex = new int[1];
-                NvrService.getInstance().time2VideoPath(
-                        playBackParam.getIp()[0], playBackParam.getIp()[1],
-                        playBackParam.getIp()[2], playBackParam.getIp()[3],
-                        playBackParam.getPort(), playBackParam.getChannel(),
-                        timeIntervals[0][0][0], timeIntervals[0][0][1], timeIntervals[0][0][2],
-                        timeIntervals[0][0][3], timeIntervals[0][0][4], timeIntervals[0][0][5],
-                        timeIntervals[0][1][0], timeIntervals[0][1][1], timeIntervals[0][1][2],
-                        timeIntervals[0][1][3], timeIntervals[0][1][4], timeIntervals[0][1][5],
-                        tsDir + File.separator + "play0.dav",
-                        new PlayCallback() {
+                    @Override
+                    public boolean onNew(RequestState requestState) {
+                        FileUtils.mkdir(tsDir);
+                        final String longTimeM3U8Path = tsDir + File.separator + CommonDefine.LONG_TIME_M3U8;
+                        FileUtils.createFile(longTimeM3U8Path);
+                        final int[][][] timeIntervals = LongTimeHelper.splitTime(
+                                playBackParam.getStartYear(), playBackParam.getStartMon(), playBackParam.getStartDay(),
+                                playBackParam.getStartHour(), playBackParam.getStartMin(), playBackParam.getStartSec(),
+                                playBackParam.getEndYear(), playBackParam.getEndMon(), playBackParam.getEndDay(),
+                                playBackParam.getEndHour(), playBackParam.getEndMin(), playBackParam.getEndSec());
+                        // 数组实现执向引用的常引用
+                        final String[] playBackPath = new String[1];
+                        final boolean[] waitEnd = new boolean[1];
+                        waitEnd[0] = false;
+                        final int[] tsIndex = new int[1];
+                        final int seq = timeIntervals.length * 2;
+                        NvrService.getInstance().time2VideoPath(
+                                playBackParam.getIp()[0], playBackParam.getIp()[1],
+                                playBackParam.getIp()[2], playBackParam.getIp()[3],
+                                playBackParam.getPort(), playBackParam.getChannel(),
+                                timeIntervals[0][0][0], timeIntervals[0][0][1], timeIntervals[0][0][2],
+                                timeIntervals[0][0][3], timeIntervals[0][0][4], timeIntervals[0][0][5],
+                                timeIntervals[0][1][0], timeIntervals[0][1][1], timeIntervals[0][1][2],
+                                timeIntervals[0][1][3], timeIntervals[0][1][4], timeIntervals[0][1][5],
+                                tsDir + File.separator + "play0.dav",
+                                new PlayCallback() {
+                                    @Override
+                                    public void onComplete(String filePath) {
+                                        String firstM3U8 = sysSingleMp42TS(filePath, 0, tsDir);
+                                        tsIndex[0] = firstM3U8(firstM3U8, longTimeM3U8Path, tsDir, seq);
+                                        playBackPath[0] = longTimeM3U8Path;
+                                        FileUtils.rm(filePath);
+                                        waitEnd[0] = true;
+                                    }
+                                });
+                        int i = 50;
+                        while (!waitEnd[0] && i > 0) {
+                            // 阻塞只为onNew返回requestState
+                            ThreadUtils.sleep(1000);
+                            i--;
+                        }
+                        VSLog.d(TAG, "convert time :" + i);
+                        if (!waitEnd[0] || !FileUtils.isExist(playBackPath[0])) {
+                            if (waitEnd[0]) {
+                                VSLog.e(TAG, "generate required file failed" + playBackPath[0]);
+                                PlaybackHelper.responseString(context.getResponse(), "generate required file failed");
+                            } else {
+                                VSLog.e(TAG, "wait for longtime over 50 seconds");
+                                PlaybackHelper.responseString(context.getResponse(), "wait for longtime over 50 seconds");
+                            }
+                            context.complete();
+                            return false;
+                        }
+                        VSLog.d(TAG, "after convert");
+                        LongTimeRes longTimeRes = new LongTimeRes(playBackPath[0].replace(CommonDefine.DATA_PATH + File.separator, ""));
+                        PlaybackHelper.responseString(context.getResponse(), longTimeRes.toJson(sessionState.getSessionId()));
+                        requestState.setRes(longTimeRes);
+                        context.complete();
+                        VSLog.d(TAG, "on Complete");
+                        ThreadUtils.runInBackGround(new Runnable() {
                             @Override
-                            public void onComplete(String filePath) {
-                                String firstM3U8 = sysSingleMp42TS(filePath, 0, tsDir);
-                                tsIndex[0] = firstM3U8(firstM3U8, longTimeM3U8Path, tsDir);
-                                playBackPath[0] = longTimeM3U8Path;
-                                FileUtils.rm(filePath);
-                                waitEnd[0] = true;
+                            public void run() {
+                                for (int i = 1; i < timeIntervals.length; i++) {
+                                    addM3U8(i, tsIndex, playBackParam, timeIntervals, tsDir, longTimeM3U8Path);
+                                }
                             }
                         });
-                int i = 50;
-                while (!waitEnd[0] && i > 0) {
-                    // 阻塞只为onNew返回requestState
-                    ThreadUtils.sleep(1000);
-                    i--;
-                }
-                VSLog.d(TAG, "convert time :" + i);
-                if (!waitEnd[0] || !FileUtils.isExist(playBackPath[0])) {
-                    if (waitEnd[0]) {
-                        VSLog.e(TAG, "generate required file failed" + playBackPath[0]);
-                        PlaybackHelper.responseString(context.getResponse(), "generate required file failed");
-                    } else {
-                        VSLog.e(TAG, "wait for longtime over 50 seconds");
-                        PlaybackHelper.responseString(context.getResponse(), "wait for longtime over 50 seconds");
-                    }
-                    context.complete();
-                    return false;
-                }
-                VSLog.d(TAG, "after convert");
-                LongTimeRes longTimeRes = new LongTimeRes(playBackPath[0].replace(CommonDefine.DATA_PATH + File.separator, ""));
-                PlaybackHelper.responseString(context.getResponse(), longTimeRes.toJson(sessionState.getSessionId()));
-                requestState.setRes(longTimeRes);
-                context.complete();
-                VSLog.d(TAG, "on Complete");
-                ThreadUtils.runInBackGround(new Runnable() {
-                    @Override
-                    public void run() {
-                        for (int i = 1; i < timeIntervals.length; i++) {
-                            addM3U8(i, tsIndex, playBackParam, timeIntervals, tsDir, longTimeM3U8Path);
-                        }
+                        return true;
                     }
                 });
-                return true;
-            }
-        });
 
     }
 
@@ -147,8 +148,9 @@ public class AsyncLongTimePlay implements Runnable {
     }
 
     public static String sysSingleMp42TS(String tmpFilePath, int index, String tsDir) {
-        CmdExecutor.wait("ffmpeg -n -i " + tmpFilePath + " -s 640x360 -f ssegment -segment_format mpegts -segment_list "
-                + tsDir + "/play" + index + ".m3u8 -vcodec libx264 " + tsDir + "/out" + index + "%03d.ts\n");
+//        CmdExecutor.wait("ffmpeg -n -i " + tmpFilePath + " -s 640x360 -f ssegment -segment_format mpegts -segment_list "
+//                + tsDir + "/play" + index + ".m3u8 -vcodec libx264 " + tsDir + "/out" + index + "%03d.ts");
+        CmdExecutor.wait("ffmpeg -n -i " + tmpFilePath + " -s 640x360 -vcodec libx264 -hls_allow_cache 1 -hls_list_size 0 -f hls " + tsDir + "/play" + index + ".m3u8");
 //        -segment_time 2
         return tsDir + "/play" + index + ".m3u8";
     }
@@ -158,42 +160,29 @@ public class AsyncLongTimePlay implements Runnable {
             if (lineContent.startsWith("#")) {
                 buf.append(lineContent);
                 buf.append("\n");
-            } else if (lineContent.startsWith("out")) {
-                buf.append("out");
+            } else if (lineContent.startsWith("play")) {
+                buf.append("play");
                 buf.append(curIndex[0]);
                 buf.append(".ts");
                 buf.append("\n");
-                FileUtils.rename(tsDir + File.separator + lineContent, tsDir + File.separator + "out" + curIndex[0] + ".ts");
+                FileUtils.rename(tsDir + File.separator + lineContent, tsDir + File.separator + "play" + curIndex[0] + ".ts");
                 curIndex[0]++;
             }
         }
         return buf;
     }
 
-    public static int firstM3U8(String partPath, String wholePath, final String tsDir) {
+    public static int firstM3U8(String partPath, String wholePath, final String tsDir, final int seq) {
         final int[] tsIndex = new int[1];
         tsIndex[0] = 0;
-        final StringBuilder buf = new StringBuilder();
-        FileUtils.readLine(partPath, new FileUtils.ReadLine() {
-            @Override
-            public void onLine(String lineContent, int lineIndex) {
-                if (lineIndex < 5) {
-                    if (lineContent.startsWith("#EXT-X-TARGETDURATION:")) {
-                        // fix for ts longer than 10 seconds
-                        buf.append("#EXT-X-TARGETDURATION:");
-                        buf.append(CommonDefine.LONG_SPLIT_INTERVAL / 1000 + 2);
-                        buf.append("\n");
-                    } else {
-                        buf.append(lineContent);
-                        buf.append("\n");
-                    }
-                } else {
-                    appendM3U8Line(buf, lineContent, tsIndex, tsDir);
-                }
-            }
-        });
-        FileUtils.append2File(wholePath, buf.toString());
-        FileUtils.rm(partPath);
+        String buf = ("#EXTM3U\n" +
+                "#EXT-X-VERSION:3\n" +
+                "#EXT-X-MEDIA-SEQUENCE:0") +
+//                seq +
+                "\n" + "#EXT-X-ALLOW-CACHE:YES\n" + "#EXT-X-TARGETDURATION:12\n";
+        FileUtils.append2File(wholePath, buf);
+        appendM3U8(partPath, wholePath, tsIndex, tsDir);
+//        FileUtils.rm(partPath);
         return tsIndex[0];
     }
 
@@ -208,7 +197,7 @@ public class AsyncLongTimePlay implements Runnable {
             }
         });
         FileUtils.append2File(wholePath, buf.toString());
-        FileUtils.rm(partPath);
+//        FileUtils.rm(partPath);
         return curIndex[0];
     }
 
